28afc861c7881a78bfc3244c7e16998b6346e7ca
OG-Mendez <uchedavid781@gmail.com>
2024-11-01 16:26:45 +0100

    Added id column

--- campuslife/serializers.py
+++ campuslife/serializers.py
@@ -5,4 +5,4 @@ from .models import Picture
 class PictureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Picture
-        fields = ['lodge_name', 'image', 'lodge_location', 'lodge_price', 'available_vacancy', 'caretaker_number']
+        fields = ['id', 'lodge_name', 'image', 'lodge_location', 'lodge_price', 'available_vacancy', 'caretaker_number']
