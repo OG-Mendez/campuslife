0642de3b4f86dca29a7943b839f88303ae4ff6f9
OG-Mendez <uchedavid781@gmail.com>
2024-12-08 14:50:27 +0100

    Added uploaded_by to the model field

--- campuslife/models.py
+++ campuslife/models.py
@@ -14,6 +14,8 @@ class Picture(models.Model):
     is_visible = models.BooleanField(default=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='uploaded_pictures', null=True, blank=True)
+
 
     def __str__(self):
         return self.lodge_name
