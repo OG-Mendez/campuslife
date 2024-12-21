abae4fecda391394fb557cc964039d65b246fc27
OG-Mendez <uchedavid781@gmail.com>
2024-12-21 15:29:26 +0100

    Allowed the development pipeline access to backend

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['campuslife-c9je.onrender.com', '127.0.0.1', 'https://www.campuslifetechnologies.com.ng',
-                 'https://angry-trixie-david-nenye-4c47ed6b.koyeb.app', '2c8e-102-90-100-1.ngrok-free.app']
+                 'https://angry-trixie-david-nenye-4c47ed6b.koyeb.app', 'https://campuslife-xmb7-david-uchennas-projects.vercel.app']
 
 
 # Application definition
@@ -71,7 +71,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://campuslife-xmb7.vercel.app",
     "https://www.campuslifetechnologies.com.ng",
-    "https://angry-trixie-david-nenye-4c47ed6b.koyeb.app"
+    "https://angry-trixie-david-nenye-4c47ed6b.koyeb.app",
+    'https://campuslife-xmb7-david-uchennas-projects.vercel.app'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
