1d5e62a4156849d71f0c37bd3631c0b05543812e
OG-Mendez <uchedavid781@gmail.com>
2025-05-29 04:06:35 +0100

    Removed the forum from codebase

--- campuslife/serializers.py
+++ campuslife/serializers.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 from rest_framework import serializers
-from .models import Picture, Interior, Rating, Review, Question, Answer, Reply, Notification, Room, Wallet, Agent, AgentEarning
+from .models import Picture, Interior, Rating, Review, Room, Wallet, Agent, AgentEarning
 
 
 class PictureSerializer(serializers.ModelSerializer):
@@ -46,49 +46,6 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     def get_lodge_id(self, obj):
         return obj.rating.picture_rating.id if obj.rating and obj.rating.picture_rating else None
-
-
-class AnswerSerializer(serializers.ModelSerializer):
-    net_score = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Answer
-        fields = ['id', 'question_replied', 'answered_by', 'content', 'created_at', 'net_score', 'total_replies']
-
-    def get_net_score(self, obj):
-        return obj.upvote_answer.count() - obj.downvote_answer.count()
-
-    def get_total_replies(self, obj):
-        return obj.total_replies.count()
-
-
-class QuestionSerializer(serializers.ModelSerializer):
-    answers = AnswerSerializer(many=True, read_only=True, source='answers.all')
-
-    class Meta:
-        model = Question
-        fields = ['id', 'asked_by', 'question', 'created_at', 'upvote_question', 'downvote_question', 'answers',
-                  'total_answers']
-
-    def get_net_score(self, obj):
-        return obj.upvote_answer.count() - obj.downvote_answer.count()
-
-    def get_total_answers(self, obj):
-        return obj.total_answers.count()
-
-
-class ReplySerializer(serializers.ModelSerializer):
-    replies = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Reply
-        fields = ['id', 'replied_by', 'answer', 'content', 'created_at', 'likes', 'dislikes']
-
-
-class NotificationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notification
-        fields = '__all__'
 
 
 class RoomSerializer(serializers.ModelSerializer):
